fc4c51883bf57853ca47e9a674ebf0cc3014aec9
juank8911 <ingjuanguzmane@gmail.com>
2026-07-30 03:03:41 -0500

    feat: initialize mobile project with Expo, dependencies, and Metro configuration

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -3,4 +3,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 config.maxWorkers = 1;
 
+// Habilitar soporte para la propiedad "exports" de los package.json
+config.resolver.unstable_enablePackageExports = true;
+
 module.exports = config;
